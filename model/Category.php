8901af8a9617cf7d5754d6e89d010246264e516b
mizandarii <kristiina.parri@ivkhk.ee>
2025-03-10 13:05:46 +0200

    NEW-18

---
+++ model/Category.php
@@ -0,0 +1,9 @@
+<?php
+Class Category{
+    public static function getAllCategory(){
+        $query = "SELECT * FROM category"
+        $db = new Database();
+        $arr = $db -> getAll($query);
+        return $arr;
+    }
+}
---
+++ model/News.php
@@ -0,0 +1,32 @@
+<?php
+class News{
+    public static function getLast10News(){
+        $query = "SELECT * from news order by id desc limit 3";
+        $db = new Database();
+        $arr = $db -> getAll($query);
+        return $arr;
+    }
+
+    public static function getAllNews(){
+        $query = "SELECT * from news order by id desc";
+        $db = new Database();
+        $arr = $db -> getAll($query);
+        return $arr;
+    }
+
+    public static function getNewsByCategoryID($id){
+        $query = "SELECT * from news where category_id = ".(string)$id." order by id desc";
+        $db = new Database();
+        $arr = $db -> getAll($query);
+        return $arr;
+    }
+
+    public static function getNewsByID($id){
+        $query = "SELECT * from news where id = ".(string)$id;
+        $db = new Database();
+        $arr = $db -> getAll($query);
+        return $arr;
+    }
+
+
+}
