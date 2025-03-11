fc36c5937d1cbda916e25c4463ba8c5fc2e69429
mizandarii <kristiina.parri@ivkhk.ee>
2025-03-11 14:13:55 +0200

    NEW-29

--- admin/viewAdmin/templates/layout.php
+++ admin/viewAdmin/templates/layout.php
@@ -12,15 +12,45 @@
 <body>
     <div class="container">
 
+    <?php
+    if (isset($_SESSION["userId"]) && isset($_SESSION["sessionId"]))
+    {
+
+    ?>
+
         <div class="header clearfix">
         <nav class="navbar navbar-default">
         <div class="container-fluid">
+        
+
+
+
+        <?php
+        echo '<ul class="nav nav-pills pull-right">
+        <li role= "button">'.$_SESSION["name"].
+            '<a href="logout" style="display: inline;">Выйти <i class="fa fa-sign-out"></i>
+        </a></li></ul>';
+
+        if (isset($_SESSION["status"]) && $_SESSION["status"]=="admin"){
+            echo '<h4?><a href="../" target= blank>WEBSITE</a>';
+            echo ' &#187 <a href="categoryAdmin">Categories</a>';
+            echo ' &#187 <a href="newsAdmin">NewsList</a>';
+
+            echo ' </h4>';
+        }else{
+            echo '<h4>У вас нет прав!</h4>';
+        }
+        ?>
 
     </div>
     </nav>
     </div>
-        <div id="content" style="padding-top:20px;">
+        <?php
+    }
+    ?>
 
+        <div id="content" style="padding-top:20px;">
+            <?php echo $content; ?>
         </div>
         <footer class="footer">
             <p>&copy; 2019 Design Admin Dashboard<i class="fa fa-child"></i></p>
