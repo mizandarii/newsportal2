3a28bf13dd493281c7df6979f06d91cd1c6a6d11
mizandarii <kristiina.parri@ivkhk.ee>
2025-03-11 14:29:39 +0200

    NEW-35

---
+++ admin/routeAdmin/routingAdmin.php
@@ -0,0 +1,24 @@
+<?php
+$host = explode('?', $_SERVER['REQUEST_URI'])[0];
+$num = substr_count($host, '/');
+$path = explode('/'. $host)[$num];
+
+if($path == '' OR $path == 'index.php')
+{
+    //main page
+    $response = controllerAdmin::formLoginSite();
+}
+elseif($path == 'login')
+{
+    //login
+    $response = controllerAdmin::loginAction();
+}
+elseif($path=='logout')
+{
+    //lpgout
+    $response = controllerAdmin::logoutAction();
+}
+else{
+    //page does not exist
+    $response = controllerAdmin::error404();
+}
