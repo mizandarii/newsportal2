bbacba7a887915e85d7d1447d73252683123216a
mizandarii <kristiina.parri@ivkhk.ee>
2025-03-11 13:25:48 +0200

    NEW-28

---
+++ admin/viewAdmin/templates/layout.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <title>Dashboard</title>
+    <link href="public/css/bootstrap.css" rel="stylesheet">
+    <link href="public/css/mystyle.css" rel="stylesheet">
+    <link href="public/css/font-awesome.min.css" rel="stylesheet">
+
+    <script src="public/js/jquery.min.js"></script>
+    <script src="public/js/bootstrap.min.js"></script>
+    <script type="text/js/javascript" src="public/js/ajaxupload.3.5.js"></script>
+</head>
+<body>
+    <div class="container">
+
+        <div class="header clearfix">
+        <nav class="navbar navbar-default">
+        <div class="container-fluid">
+
+    </div>
+    </nav>
+    </div>
+        <div id="content" style="padding-top:20px;">
+
+        </div>
+        <footer class="footer">
+            <p>&copy; 2019 Design Admin Dashboard<i class="fa fa-child"></i></p>
+        </footer>
+    </div>
+</body>
+</html>
